5c6b69d574592d505029ec7fd227e9bb5e1f6c86
andychen <descartes1960s@gmail.com>
2023-11-01 13:08:00 +0800

    style: card name 新增刪節號 & 圖片優化

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import pointerGrey from "../assets/images/icons/pointer-grey.svg";
+import { useNavigate } from "react-router-dom";
+import NoImage from "../assets/images/NoImage-255x200.png";
 const Card = ({ id, city, name, type, img }) => {
+  const [imgIsLoading, setImgIsLoading] = useState(true);
+  let words = name.split("");
+  if (words.length > 10) {
+    name = words.slice(0, 15).join("") + "...";
+  }
+  const navigate = useNavigate();
   return (
-    <div>
-      <a href={`/${type}/${id}`}>
-        <img src={img} className="card-img-general" alt="card-img" />
-      </a>
-      <div className="description">
+    <div className="my-2">
+      <div
+        className="description cursor-pointer"
+        onClick={() => navigate(`/${type}/${id}`)}
+      >
+        <div className="wrap overflow-hidden rounded">
+          <img
+            src={imgIsLoading ? NoImage : img}
+            onLoad={() => setImgIsLoading(false)}
+            className="card-img-general"
+            alt="card-img"
+          />
+        </div>
         <div className="fw-bold h5">{name}</div>
         <div className="text-secondary">
           <img src={pointerGrey} alt="pointer-icon" />
