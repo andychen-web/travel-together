3e77742fe965470719a789e226840abd30bb4091
andychen <descartes1960s@gmail.com>
2023-11-02 16:09:52 +0800

    refactor: update card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,23 +1,34 @@
 import React, { useState } from "react";
 import pointerGrey from "../assets/images/icons/pointer-grey.svg";
 import { useNavigate } from "react-router-dom";
-import NoImage from "../assets/images/NoImage-255x200.png";
-const Card = ({ id, city, name, type, img }) => {
+import noImage from "../assets/images/no_picture_activity.jpg";
+const Card = ({ item, id, city, name, category, img }) => {
   const [imgIsLoading, setImgIsLoading] = useState(true);
+  let shownImg;
+  if (img?.endsWith("jpg") || img?.endsWith(".png") || img?.endsWith(".gif")) {
+    shownImg = img;
+  } else {
+    shownImg = noImage;
+  }
   let words = name.split("");
   if (words.length > 10) {
     name = words.slice(0, 15).join("") + "...";
   }
   const navigate = useNavigate();
+  const handleCardClick = () => {
+    console.log(id, JSON.stringify(item));
+    localStorage.setItem(id, JSON.stringify(item));
+    navigate(`/${category}/${id}`);
+  };
   return (
     <div className="my-2">
       <div
         className="description cursor-pointer"
-        onClick={() => navigate(`/${type}/${id}`)}
+        onClick={() => handleCardClick()}
       >
         <div className="wrap overflow-hidden rounded">
           <img
-            src={imgIsLoading ? NoImage : img}
+            src={imgIsLoading ? noImage : shownImg}
             onLoad={() => setImgIsLoading(false)}
             className="card-img-general"
             alt="card-img"
