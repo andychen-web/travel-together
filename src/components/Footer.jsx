9bcfdf91a749194b52d55eb9b78bc3f669daff06
andychen <descartes1960s@gmail.com>
2023-11-02 14:27:05 +0800

    style: 新增footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer bg-primary text-white p-3 text-center">
+      The F2E 3rd 台灣旅遊景點導覽
+    </footer>
+  );
+};
+
+export default Footer;
