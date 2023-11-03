1c356993f36f6404581c64104cfa34cad5f8597d
andychen <descartes1960s@gmail.com>
2023-11-03 16:04:09 +0800

    fix: breadcrumb href

--- src/components/MyBreadCrumb.jsx
+++ src/components/MyBreadCrumb.jsx
@@ -1,13 +1,22 @@
 import Breadcrumb from "react-bootstrap/Breadcrumb";
+import { useLocation } from "react-router-dom";
 const MyBreadcrumb = ({ routes }) => {
+  const location = useLocation();
+  const typeRoute = location.pathname.split("/")[1];
   return (
     <Breadcrumb>
       <Breadcrumb.Item href="/">首頁</Breadcrumb.Item>
-      {routes.map((route, index) => (
-        <Breadcrumb.Item active key={index}>
-          {route}
-        </Breadcrumb.Item>
-      ))}
+      {routes.map((route, index) => {
+        return (
+          <Breadcrumb.Item
+            active={index === routes.length - 1 ? "true" : ""}
+            key={index}
+            href={"/" + typeRoute}
+          >
+            {route}
+          </Breadcrumb.Item>
+        );
+      })}
     </Breadcrumb>
   );
 };
