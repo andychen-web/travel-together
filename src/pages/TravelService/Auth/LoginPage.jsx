ad27e146ce6142c7f804ae75b0271863e91aae37
andychen <andychenweb@gmail.com>
2024-12-10 15:04:43 +0800

    t

--- src/pages/TravelService/Auth/LoginPage.jsx
+++ src/pages/TravelService/Auth/LoginPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 import OauthContainer from "./OauthContainer";
-import {useMutation,} from "@tanstack/react-query";
+// import {useMutation,} from "@tanstack/react-query";
 import { Link, useNavigate, useLocation } from "react-router-dom";
 import { apiLogin } from "@/api-client";
 import { useTranslation } from "react-i18next";
